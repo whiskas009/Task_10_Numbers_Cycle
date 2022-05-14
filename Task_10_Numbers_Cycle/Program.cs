6b5414d27ce47739bcc50c1e50b6d5622e7c61f2
whiskas009 <76867818+whiskas009@users.noreply.github.com>
2022-05-14 23:11:00 +0300

    #1

---
+++ Task_10_Numbers_Cycle/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_10_Numbers_Cycle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int number = 5; number < 100; number += 7)
+            {
+                Console.WriteLine(number);
+            }
+        }  
+    }
+}
